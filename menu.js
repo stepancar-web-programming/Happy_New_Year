94554f628df3cf610462d6d4312857869dda17b9
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-28 22:20:01 +0300

    исправил отображение меню

--- menu.js
+++ menu.js
@@ -12,10 +12,10 @@ for (const smoothLink of smoothLinks) {
 }
 function openNav() {
   document.getElementById('mySidenav').style.width = '250px';
-  document.getElementById('main').style.marginLeft = '250px';
+  document.getElementById('main').style.marginRight = '250px';
 }
 
 function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
-  document.getElementById('main').style.marginLeft = '0';
+  document.getElementById('main').style.marginRight = '0';
 }
