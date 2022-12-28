a8513b533060f02da8b296a854ffa931bbc030e4
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-28 23:40:04 +0300

    исправил - _

--- garland.js
+++ garland.js
@@ -1,20 +1,20 @@
 function garland() {
-  const nums = document.getElementById('nums-1').innerHTML;
+  const nums = document.getElementById('nums_1').innerHTML;
   if (nums == 1) {
-    document.getElementById('garland').className = 'garland-1';
-    document.getElementById('nums-1').innerHTML = '2';
+    document.getElementById('garland').className = 'garland_1';
+    document.getElementById('nums_1').innerHTML = '2';
   }
   if (nums == 2) {
-    document.getElementById('garland').className = 'garland-2';
-    document.getElementById('nums-1').innerHTML = '3';
+    document.getElementById('garland').className = 'garland_2';
+    document.getElementById('nums_1').innerHTML = '3';
   }
   if (nums == 3) {
-    document.getElementById('garland').className = 'garland-3';
-    document.getElementById('nums-1').innerHTML = '4';
+    document.getElementById('garland').className = 'garland_3';
+    document.getElementById('nums_1').innerHTML = '4';
   }
   if (nums == 4) {
-    document.getElementById('garland').className = 'garland-4';
-    document.getElementById('nums-1').innerHTML = '1';
+    document.getElementById('garland').className = 'garland_4';
+    document.getElementById('nums_1').innerHTML = '1';
   }
 }
 
